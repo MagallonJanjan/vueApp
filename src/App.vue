58ac4e051c13ba79db4a3c49452d576bba2ae94e
AnkleBreaker <MagallonJanjan@gmail.com>
2021-04-09 20:44:00 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+      <registration />
+  </div>
+</template>
+
+<script>
+
+import Registration from './components/Auth/Registration.vue';
+
+export default {
+  name: 'App',
+  components: {
+    Registration
+  }
+}
+</script>
+
---
+++ src/components/Auth/Registration.vue
@@ -0,0 +1,17 @@
+<template>
+  
+</template>
+
+<script>
+export default {
+    name: 'Registration'
+}
+</script>
+
+
+
+
+<style scoped>
+
+
+</style>
